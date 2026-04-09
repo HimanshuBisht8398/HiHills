6de9cf733743c199f39a76287363df3d4c66d89d
Himanshu Bisht <himanshubonov8.com>
2026-04-09 11:11:42 +0530

    changes

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,4 +1,4 @@
-const DEFAULT_API_BASE_URL = "http://localhost:4000";
+const DEFAULT_API_BASE_URL = "https://hihillsbackend-production.up.railway.app";
 
 export const getApiUrl = (path: string) => {
   const baseUrl =
